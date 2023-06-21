76fa913617e9a901a38b593d9349912d06524027
Ryan Quey <quey.ryan@gmail.com>
2023-06-21 12:02:34 +0700

    I guess only now the python class changes got added

--- chatgpt/main.py
+++ chatgpt/main.py
@@ -5,88 +5,158 @@ from pathlib import Path
 load_dotenv()
 base_path = Path(__file__).parent
 
-#file_path = (base_path / "../sample-text/sample-commentary-text.2.8-15.unchanged.md").resolve()
+class SimplifyJob:
 
-#file_path = (base_path / "./sample-text/sample-commentary-text.2.8-15.unchanged.txt").resolve()
-file_path = (base_path / "./sample-text/sample-commentary-text.2.8-15.quotes-for-scr.txt").resolve()
+    testing = False
+    #testing = True
+    print(f"testing: {testing}")
 
-results_file_path = (base_path / "./results/Calvin.Colossians.2.8.GPT-result.txt")
+    ####################
+    # Files to send through
+    ####################
+    # see Ryan's Obsidian notes for what these version numbers refer to.
+    formatted_base_text_version = "2.4"
+    ####
+    if testing:
+        # file_path = (base_path / f"./sample-text/sample-commentary-text.2.8-15.from-{formatted_base_text_version}.quotes-for-scr.txt").resolve()
+        # for using markdown instead
+        file_path = (base_path / f"./sample-text/sample-commentary-text.2.8-15.from-{formatted_base_text_version}.quotes-for-scr.md").resolve()
+    else:
+        # file_path = (base_path / f"./original-text/Calvin.Colossians.en.{formatted_base_text_version} - ch.1.md").resolve()
+        file_path = (base_path / f"./sample-text/sample-commentary-text.2.8-15.from-{formatted_base_text_version}.quotes-for-scr.md").resolve()
+        # file_path = (base_path / f"./original-text/Calvin.Colossians.en.{formatted_base_text_version} - ch.2.md").resolve()
+        # file_path = (base_path / f"./original-text/Calvin.Colossians.en.{formatted_base_text_version} - ch.3.md").resolve()
+        # file_path = (base_path / f"./original-text/Calvin.Colossians.en.{formatted_base_text_version} - ch.4.md").resolve()
 
+    ####################
+    # Result files
+    ####################
+    ####
+    conversion_script_version = "0.0.5.3"
+    # For testing
+    # (depending on how big of a chunk)
 
-#f = open("demofile.txt", "r")
-
-
-# shouldn't need, using API key
-#openai.api_key = "sk-..."
-
-# list models
-models = openai.Model.list()
-
-# print the first model's id
-#print("all models", models.data)
-#print("first model", models.data[0].id)
-
-# experimenting with different apis. 
-api = "chat"
-#api = "completion"
-
-please_continue = "Please continue"
-
-# TODO try different propmts
-# Nathan's original
-#prompt_base = "Update the language used in this ancient commentary to modern English:"
-# 0.0.3.1
-#prompt_base = "Update the language used in John Calvin's Bible commentary to modern English:"
-# 0.0.3.2
-#prompt_base = "Update the language used in John Calvin's Bible commentary to simple, modern English:"
-# 0.0.3.3
-#prompt_base = "Update the language used in John Calvin's Bible commentary to clear, modern English:"
-# 0.0.3.4
-prompt_base = "Update the language used in John Calvin's Colossians Bible commentary to clear, modern English:"
-# TODO not yet tried
-#prompt_base = "Update the language used in this ancient Bible commentary to modern English:"
-# TODO not yet tried
-#prompt_base = "Update the language used in this ancient commentary in markdown to modern English:"
+    ####
+    if testing:
+        results_file_path = (base_path / f"./results/Col.2.8a_v.{conversion_script_version}.txt")
+        #results_file_path = (base_path / f"./results/Col.2.8-9_v.{conversion_script_version}.txt")
+    else:
+        results_file_path = (base_path / f"./results/Colossians.whole_v.{conversion_script_version}.txt")
 
 
+    # shouldn't need, using API key set by env var. (see .env file)
+    #openai.api_key = "sk-..."
 
-# TODO try different dividers
-divider = "\n"
-#divider = "\n\n"
-#divider = " "
+    if testing:
+        # list models
+        models = openai.Model.list()
 
-# TODO experiment with different blocks, to make sure results are repeatable. 
+        # print the first model's id
+        #print("all models", models.data)
+        print("first model available in OpenAI's API:", models.data[0].id)
 
-n_lines = 7
-# try all
-#n_lines = 2500
+    # experimenting with different apis. 
+    api = "chat"
+    #api = "completion"
 
-final_result_arr = []
-has_more_to_say = False
-messages = []
+    please_continue = "Please continue"
 
-def start_conversation():
-    with open(file_path, "r") as f:
+    # TODO try different propmts
+    # Nathan's original
+    #prompt_base = "Update the language used in this ancient commentary to modern English:"
+    # TODO not yet tried
+    #prompt_base = "Update the language used in this ancient Bible commentary to modern English:"
+    # TODO not yet tried
+    #prompt_base = "Update the language used in this ancient commentary in markdown to modern English:"
 
-        # get only the top n lines
-        head = [next(f) for _ in range(0, n_lines)]
+    # 0.0.3.1
+    #prompt_base = "Update the language used in John Calvin's Bible commentary to modern English:"
+    # 0.0.3.2
+    #prompt_base = "Update the language used in John Calvin's Bible commentary to simple, modern English:"
+    # 0.0.3.3
+    #prompt_base = "Update the language used in John Calvin's Bible commentary to clear, modern English:"
+    # 0.0.3.4 -- 0.0.4.2
+    #prompt_base = "Update the text delimited by triple quotes used in John Calvin's Colossians Bible commentary to clear, modern English:"
 
-        commentary_text_block = "\n".join(head)
-        #print("original text block:\n\n", head)
+    # 0.0.5.1-______  (currently using)
+    prompt_base = "Please help to update the following markdown text delimited by triple quotes, which was taken from John Calvin's Colossians Bible commentary:"
 
-        prompt = f"{prompt_base}{divider}{commentary_text_block}"
-        print("Using prompt:\n\n", prompt)
 
-        messages.append({"role": "user", 
-                         "content": prompt
-                       })
+    # TODO experiment with different blocks, to make sure results are repeatable. 
+    # - Note that longer blocks take longer to get response, and eat up more quota. 
+
+    final_result_arr = []
+    has_more_to_say = False
+    messages = []
+
+    prompt_system = True
+
+    # https://platform.openai.com/docs/guides/gpt-best-practices/tactic-ask-the-model-to-adopt-a-persona
+    if prompt_system:
+        print("adding system prompt")
+
+        # 0.0.5.1 - Just leaving for now
+        #system_text = "When I ask for help to update a markdown text, update to clear, modern English:"
+        # 0.0.5.2 High school reading level
+        # system_text = "When I ask for help to update a markdown text, update to clear, modern English at a high school reading level:"
+        # 0.0.5.3 ESL reading level
+        system_text = "When I ask for help to update a markdown text, update to clear, modern English for ESL reading level:"
+        # 0.0.5.3.2 ESL reading level - remove passive
+        #system_text = "When I ask for help to update a markdown text, update to clear, modern English for ESL reading level (when possible use active voice instead of passive voice):"
+        # 0.0.5.4 Junior High school reading level
+        # system_text = "When I ask for help to update a markdown text, update to clear, modern English at a junior high school reading level:"
+        messages.append({
+            "role": "system", 
+            "content": system_text 
+        })
+
+    commentary_text_block = None
+
+    def get_original_text(self):
+
+
+        with open(self.file_path, "r") as f:
+
+            # get only the top n lines
+            if self.testing:
+                # n_lines = 3
+                # 5 lines gets the first part of 2:8, which is enough for quick iteration/testing
+                n_lines = 5
+
+                # 19 lines gets 2:8-9
+                # n_lines = 19
+
+                text = [next(f) for _ in range(0, self.n_lines)]
+
+            else:
+                text = f.readlines()
+
+            self.commentary_text_block = "\n".join(text)
+            #print("original text block:\n\n", text)
+
+            # Use delimiters to clearly indicate distinct parts of the input
+            # https://platform.openai.com/docs/guides/gpt-best-practices/six-strategies-for-getting-better-results
+            self.prompt = f"{self.prompt_base}\n\"\"\"{self.commentary_text_block}\"\"\""
+
+            self.messages.append({
+                "role": "user", 
+                "content": self.prompt
+            })
+            #print("Using prompt:\n\n", self.prompt)
+
+    def start_conversation(self):
+        """
+        Start the conversation with the chat bot.
+        """
+
+        print("Using messages:\n\n", self.messages)
 
         print("now sending to GPT API...")
         # create a chat completion
-        if api == "chat":
+        if self.api == "chat":
             chat_completion = openai.ChatCompletion.create(
                 model="gpt-4", 
-                messages=messages
+                messages=self.messages
                 )
 
             print(chat_completion.choices)
@@ -94,53 +164,78 @@ def start_conversation():
             # for now, just taking first choice only
             assistant_message = chat_completion.choices[0].message
             response = assistant_message.content
-            has_more_to_say = chat_completion.choices[0].finish_reason == "length"
+
+            # if the chat bot finished because of length issue, then that means it has "more to say".
+            self.has_more_to_say = chat_completion.choices[0].finish_reason == "length"
 
             # append result, in case this conversation needs to continue
             # TODO I think if we extend our token limit, we might not ever need to do this in the API? Check, would make things simpler.
-            messages.append(assistant_message)
+            self.messages.append(assistant_message)
 
         else:
             # note that gpt-4 is a chat model, so doesn't work with Completion api
             # TODO never tested
             completion = openai.Completion.create(
                 model="text-davinci-003", 
-                prompt=prompt
+                prompt=self.prompt
                 )
             print(completion.choices)
             response = completion.choices[0].message.content
 
         # print the chat completion
         print("Response for round:", response)
-        final_result_arr.append(response) 
 
-start_conversation()
+        self.final_result_arr.append(response) 
 
-if has_more_to_say:
-    # append 
-    messages.append({"role": "user", "content": please_continue})
-    print("now sending another round to GPT API...(please wait)")
-    next_chat_completion = openai.ChatCompletion.create(
-        model="gpt-4", 
-        messages=messages
-        )
+    def get_more(self):
+        """
+        - if need to request more results from server.
+        """
+        # append 
+        self.messages.append({"role": "user", "content": please_continue})
+        print("now sending another round to GPT API...(please wait)")
+        next_chat_completion = openai.ChatCompletion.create(
+            model="gpt-4", 
+            messages=self.messages
+            )
 
-    # TODO make this into a loop, incase we have to do this multiple times. 
-    assistant_message = next_chat_completion.choices[0].message
-    response = assistant_message.content
-    has_more_to_say = next_chat_completion.choices[0].finish_reason == "length"
+        # TODO make this into a loop, incase we have to do this multiple times. 
+        assistant_message = next_chat_completion.choices[0].message
+        response = assistant_message.content
+        self.has_more_to_say = next_chat_completion.choices[0].finish_reason == "length"
 
-    # append result, in case this conversation needs to continue
-    # TODO I think if we extend our token limit, we might not ever need to do this in the API? Check, would make things simpler.
-    messages.append(assistant_message)
-    print("Response for round:", response)
-    final_result_arr.append(response) 
+        # append result, in case this conversation needs to continue
+        # TODO I think if we extend our token limit, we might not ever need to do this in the API? Check, would make things simpler.
+        self.messages.append(assistant_message)
+        print("Response for round:", response)
 
-final_result_text = "\n".join(final_result_arr)
-print("\n\n=== FINAL RESULT===\n\n")
+        self.final_result_arr.append(response) 
 
-print(final_result_text)
+    def write_to_file(self):
+        """
+        Take final results and save to disk.
+        """
+        # turn from array into string with new lines. This seems to be preferred way in Python to write to file. 
+        self.final_result_text = "\n".join(self.final_result_arr)
 
-print(f"\n\n=== Writing to file: {results_file_path} ===\n\n")
-with open(results_file_path, 'a') as the_file:
-        the_file.write(final_result_text)
+        print("\n\n=== FINAL RESULT===\n\n")
+        print(self.final_result_text)
+        print(f"\n\n=== Writing to file: {self.results_file_path} ===\n\n")
+
+        with open(self.results_file_path, 'w') as the_file:
+            the_file.write("# Original Text:\n")
+            the_file.write(self.commentary_text_block)
+
+            the_file.write("\n\n# Simplified Text:\n")
+            the_file.write(self.final_result_text)
+
+
+if __name__ == "__main__":
+    job = SimplifyJob()
+    job.get_original_text()
+    job.start_conversation()
+
+    if job.has_more_to_say:
+        job.get_more()
+
+    job.write_to_file()
